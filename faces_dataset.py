a91a316b5f6a6c7b7ba32889430814da4b35fbe2
deanzadok <nir.diamant@rishon.csf.technion.ac.il>
2018-10-03 23:35:40 +0300

    added cwgan files

---
+++ faces_dataset.py
@@ -0,0 +1,87 @@
+import csv
+import numpy as np
+import torch
+from torch.utils.data.dataset import Dataset
+from PIL import Image
+import matplotlib.pyplot as plt
+
+#####   Dataset for Face images with beauty rates   #####
+# Each entry will contain:                              #
+# Face image                                            #
+# List of 60 beauty grades in the range of [1,5]        #
+
+raters_number = 60
+
+class FacesDataset(Dataset):
+    
+    # lists to store dataset:
+    images = [] # each var is a string of image name
+    beauty_rates = [] # each var is numpy in size of 60 with floats in range of [0,1]
+    
+    def __init__(self, folder_dataset, transform=None):
+        self.transform = transform
+        
+        # Dictionary to load dataset
+        # key: image name
+        # value: list of 60 beauty rates from raters
+        dataset_dict = {}
+        
+        # read raters csv file
+        with open('./' + folder_dataset + '/All_Ratings.csv', 'r') as csvfile:
+
+            raw_dataset = csv.reader(csvfile, delimiter=',', quotechar='|')
+            for i, row in enumerate(raw_dataset):
+                row = ','.join(row)
+                row = row.split(',')
+                
+                # create list of rates for each image
+                if row[1] in dataset_dict:
+                    dataset_dict[row[1]][0].append(float(row[2]))
+                else:
+                    dataset_dict[row[1]] = [[float(row[2])]]
+
+        # move dict to lists, convert beauty rates to numpy ranged in [0,1]
+        for key, value in dataset_dict.iteritems():
+            self.images.append('./' + folder_dataset + '/Images/' + key)
+            self.beauty_rates.append((np.asarray(value, dtype=np.float32) / 5.0))
+        #print("beauty rates size: ")
+        #print(len(self.beauty_rates))
+
+    # Override to give PyTorch access to any image on the dataset
+    def __getitem__(self, index):
+        
+        img = Image.open(self.images[index])
+        #img = img.convert('RGB') #TODO: check if necessary
+        
+        # perform transform only on the image (!!)
+        if self.transform is not None:
+            img = self.transform(img)
+
+        # Convert image and beauty rates to torch tensors
+        img = torch.from_numpy(np.asarray(img))
+        features = torch.from_numpy(np.asarray(self.beauty_rates[index]).reshape([1,raters_number]))
+        # print("shape of features: ")
+        # print(features.shape())
+        Beauty_level = np.asarray(self.beauty_rates[index]).reshape([1,raters_number]).mean()
+
+        #return img, features, Is_Beauty
+        return img, features, Beauty_level
+
+    # Override to give PyTorch size of dataset
+    def __len__(self):
+        return len(self.images)
+
+if __name__ == "__main__":
+    
+    train_dataset = FacesDataset('beauty_dataset_labeled')
+
+    # sample one image and beauty rates to test correlation
+    image, features, Beauty_level = train_dataset.__getitem__(4)
+    
+    print("beauty rates: "+ str(features))
+    print("beauty rate mean: "+ str(features.mean()))
+    print("Beauty_level : " + str(Beauty_level))
+    
+    # display image (wont work on ssh)
+    # plt.imshow(image)
+    # plt.show()
