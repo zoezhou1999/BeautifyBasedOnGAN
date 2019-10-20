4a585b3e13bf70be9d3d94c5baa29c656c4b9727
ZYHZ100 <603781431@qq.com>
2019-10-20 16:34:26 +0800

    add path

--- deploy/test.py
+++ deploy/test.py
@@ -7,8 +7,8 @@ import numpy as np
 parser = argparse.ArgumentParser(description='face model test')
 # general
 parser.add_argument('--image-size', default='112,112', help='')
-parser.add_argument('--model', default='', help='path to load model.')
-parser.add_argument('--ga-model', default='', help='path to load model.')
+parser.add_argument('--model', default='/home/hxz/GraduationProject/arcface/models/model-r50-am-lfw/model,15', help='path to load model.')
+parser.add_argument('--ga-model', default='/home/hxz/GraduationProject/arcface/models/gamodel-r50/model,0', help='path to load model.')
 parser.add_argument('--gpu', default=0, type=int, help='gpu id')
 parser.add_argument('--det', default=0, type=int, help='mtcnn option, 1 means using R+O, 0 means detect from begining')
 parser.add_argument('--flip', default=0, type=int, help='whether do lr flip aug')
