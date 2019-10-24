912a06236c4a35edecb7c9b8b6c045a5e321f86e
ZYHZ100 <603781431@qq.com>
2019-10-24 21:50:32 +0800

    fixbug

--- identity_prediction/facenet.py
+++ identity_prediction/facenet.py
@@ -17,12 +17,11 @@ class FaceNet():
         self.model_path = model_path
         config = tf.ConfigProto(allow_soft_placement=True, log_device_placement=True)
         config.gpu_options.allow_growth = True
-        self.session = tf.Session(config=config)
         graph = self.load_pb(self.model_path)
+        self.session = tf.Session(config=config,graph = graph)
         self.input = graph.get_tensor_by_name('input:0')
         self.output = graph.get_tensor_by_name('embeddings:0')
         self.phase_train_placeholder = graph.get_tensor_by_name("phase_train:0")
-        self.session.graph = graph
 
     def load_pb(self, path_to_pb):
         with tf.gfile.GFile(path_to_pb, "rb") as f:
