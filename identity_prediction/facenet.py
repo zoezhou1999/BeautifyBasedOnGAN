3b0a6d8b6277f8807fe23344821fa8be6f9e23bd
ZYHZ100 <603781431@qq.com>
2019-10-26 20:12:36 +0800

    bug

--- identity_prediction/facenet.py
+++ identity_prediction/facenet.py
@@ -15,42 +15,36 @@ class FaceNet():
 
     def __init__(self, model_path):
         self.model_path = model_path
-        # config = tf.ConfigProto(allow_soft_placement=True, log_device_placement=True)
-        # config.gpu_options.allow_growth = True
-        # self.session = tf.Session(config=config, graph=self.graph)
-
-        # phase_train_placeholder = tf.placeholder(tf.bool, name='phase_train')
-
-    def load_model(self, path_to_pb, input_map):
-        with tf.gfile.GFile(path_to_pb, "rb") as f:
-            graph_def = tf.GraphDef()
-            graph_def.ParseFromString(f.read())
-        with tf.Graph().as_default() as graph:
-            tf.import_graph_def(graph_def, name='')
-            return graph
 
     def predict(self, input_batch):
-        # img = cv2.imread(img_path)
-        # embed = self.session.run(self.output, feed_dict={self.input: self.preprocess_img(img), self.phase_train_placeholder: False})
-        # embed = normalize(embed)
-        # embed = embed.reshape((512,))
-        # config = tf.ConfigProto(allow_soft_placement=True, log_device_placement=True)
-        # config.gpu_options.allow_growth = True
-        # self.session = tf.Session(config=config, graph=self.graph)
-        batch_size_placeholder = tf.placeholder(tf.int32, name='batch_size')
-        # minibatch0, channel1, height2, width3
         input_batch=tf.transpose(input_batch, [0, 2, 3, 1])
         input_batch=tf.image.resize(input_batch,(160,160))
         input_batch=(tf.cast(input_batch, tf.float32) - 127.5) / 128.0
-        input_map = {'image_batch': input_batch, 'phase_train': False} 
-        self.graph = self.load_model(self.model_path, input_map=input_map)
-        # self.phase_train_placeholder = self.graph.get_tensor_by_name("phase_train:0")
-        # Get output tensor
-        embeddings = self.graph.get_tensor_by_name("embeddings:0")
-        print('embeddings.shape:')
-        print(embeddings.shape)
-        return tf.math.l2_normalize(embeddings)
+        input_map = {'image_batch': input_batch, 'phase_train': False}
+        with tf.gfile.GFile(self.model_path, "rb") as f:
+            graph_def = tf.GraphDef()
+            graph_def.ParseFromString(f.read())
+        with tf.Graph().as_default() as graph:
+            tf.import_graph_def(graph_def, input_map=input_map, name='')
+            # Get output tensor
+            embeddings = graph.get_tensor_by_name("embeddings:0")
+            print('embeddings.shape:')
+            print(embeddings.shape)
+            return tf.math.l2_normalize(embeddings)
 
+    # config = tf.ConfigProto(allow_soft_placement=True, log_device_placement=True)
+    # config.gpu_options.allow_growth = True
+    # self.session = tf.Session(config=config, graph=self.graph)
+
+    # img = cv2.imread(img_path)
+    # embed = self.session.run(self.output, feed_dict={self.input: self.preprocess_img(img), self.phase_train_placeholder: False})
+    # embed = normalize(embed)
+    # embed = embed.reshape((512,))
+    # config = tf.ConfigProto(allow_soft_placement=True, log_device_placement=True)
+    # config.gpu_options.allow_growth = True
+    # self.session = tf.Session(config=config, graph=self.graph)
+    # batch_size_placeholder = tf.placeholder(tf.int32, name='batch_size')
+    # minibatch0, channel1, height2, width3
     #
     #
     # # Get TensorFlow expression(s) for the output(s) of this network, given the inputs.
@@ -68,8 +62,6 @@ class FaceNet():
     #         out_expr = [out_expr] if is_tf_expression(out_expr) else list(out_expr)
     #     return out_expr
     #
-
-
     # def preprocess_img(self, x):
     #     x = cv2.resize(x, (160, 160))
     #     y = (np.float32(x) - 127.5) / 128.0
