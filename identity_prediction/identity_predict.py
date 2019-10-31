e3220a4ea4022330ec40c7fffb35429e5e8cfec3
ZYHZ100 <603781431@qq.com>
2019-10-31 19:06:23 +0800

    bug

--- identity_prediction/identity_predict.py
+++ identity_prediction/identity_predict.py
@@ -12,10 +12,10 @@ import cv2
 from sklearn.preprocessing import normalize
 
 def load_pb(path_to_pb):
-    with tf.io.gfile.GFile(path_to_pb, "rb") as f:
-        graph_def = tf.compat.v1.GraphDef()
+    with tf.gfile.GFile(path_to_pb, "rb") as f:
+        graph_def = tf.GraphDef()
         graph_def.ParseFromString(f.read())
-    with tf.compat.v1.Graph().as_default() as graph:
+    with tf.Graph().as_default() as graph:
         tf.import_graph_def(graph_def, name='')
         return graph
 
