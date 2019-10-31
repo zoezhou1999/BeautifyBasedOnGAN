5fcc031b6cd69dfe82a9f9bcc3fe2429dd2d61a2
ZYHZ100 <603781431@qq.com>
2019-10-31 19:00:03 +0800

    bug

--- identity_prediction/identity_predict.py
+++ identity_prediction/identity_predict.py
@@ -13,7 +13,7 @@ from sklearn.preprocessing import normalize
 
 def load_pb(path_to_pb):
     with tf.io.gfile.GFile(path_to_pb, "rb") as f:
-        graph_def = tf.GraphDef()
+        graph_def = tf.compat.v1.GraphDef()
         graph_def.ParseFromString(f.read())
     with tf.compat.v1.Graph().as_default() as graph:
         tf.import_graph_def(graph_def, name='')
