eedcd2db64c8959966617ddf35dc1c42c9ba9fb7
ZYHZ100 <603781431@qq.com>
2019-10-31 18:59:00 +0800

    bug

--- identity_prediction/identity_predict.py
+++ identity_prediction/identity_predict.py
@@ -15,7 +15,7 @@ def load_pb(path_to_pb):
     with tf.io.gfile.GFile(path_to_pb, "rb") as f:
         graph_def = tf.GraphDef()
         graph_def.ParseFromString(f.read())
-    with tf.Graph().as_default() as graph:
+    with tf.compat.v1.Graph().as_default() as graph:
         tf.import_graph_def(graph_def, name='')
         return graph
 
