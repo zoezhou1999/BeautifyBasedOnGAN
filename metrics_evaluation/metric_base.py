ecb77b798c91d6ec3767e7b1d1100dc2a1e56425
ZYHZ100 <603781431@qq.com>
2020-02-11 22:47:35 +0800

    add

--- metrics_evaluation/metric_base.py
+++ metrics_evaluation/metric_base.py
@@ -22,7 +22,7 @@ import dataset
 #----------------------------------------------------------------------------
 # Standard metrics.
 
-fid50k = dnnlib.EasyDict(func_name='metrics.frechet_inception_distance.FID', name='fid50k', num_images=50000, minibatch_per_gpu=8)
+fid50k = dnnlib.EasyDict(func_name='metrics_evaluation.frechet_inception_distance.FID', name='fid50k', num_images=50000, minibatch_per_gpu=8)
 # ppl_zfull = dnnlib.EasyDict(func_name='metrics.perceptual_path_length.PPL', name='ppl_zfull', num_samples=100000, epsilon=1e-4, space='z', sampling='full', minibatch_per_gpu=16)
 # ppl_wfull = dnnlib.EasyDict(func_name='metrics.perceptual_path_length.PPL', name='ppl_wfull', num_samples=100000, epsilon=1e-4, space='w', sampling='full', minibatch_per_gpu=16)
 # ppl_zend = dnnlib.EasyDict(func_name='metrics.perceptual_path_length.PPL', name='ppl_zend', num_samples=100000, epsilon=1e-4, space='z', sampling='end', minibatch_per_gpu=16)
