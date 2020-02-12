d3d00baf0c1b28017876a97351094612bd0bb2fc
ZYHZ100 <603781431@qq.com>
2020-02-12 15:10:15 +0800

    add

--- selectimages.py
+++ selectimages.py
@@ -9,5 +9,15 @@ while True:
     index=random.randint(0,69999)
     if index not in indices:
         indices.append(index)
-        subprocess.check_output(['bash', '-c', "cp "+os.path.join("./datasets/ffhq","%05d.png" % index)+" ./datasets/ffhq_selected/"])
         i=i+1
+        if i<=100:
+            subprocess.check_output(['bash', '-c', "cp "+os.path.join("../datasets/ffhq_128x128/img","%05d.png" % index)+" ../datasets/ffhq_selected_1/"])
+        elif i<=200:
+            subprocess.check_output(['bash', '-c', "cp "+os.path.join("../datasets/ffhq_128x128/img","%05d.png" % index)+" ../datasets/ffhq_selected_2/"])
+        elif i<=300:
+            subprocess.check_output(['bash', '-c', "cp "+os.path.join("../datasets/ffhq_128x128/img","%05d.png" % index)+" ../datasets/ffhq_selected_3/"])
+        else:
+            subprocess.check_output(['bash', '-c', "cp "+os.path.join("../datasets/ffhq_128x128/img","%05d.png" % index)+" ../datasets/ffhq_selected_4/"])
+
+
+        
