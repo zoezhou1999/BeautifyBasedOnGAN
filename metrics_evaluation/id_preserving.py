bf7d3d82285acf45dc0e1dd37bcef5ad060dd187
ZYHZ100 <603781431@qq.com>
2020-02-13 12:26:59 +0800

    add

--- metrics_evaluation/id_preserving.py
+++ metrics_evaluation/id_preserving.py
@@ -112,7 +112,7 @@ with open(args.csv_name+ ".csv", mode='w') as f:
     writer.writerow(['image_name', 'squared l2 distance'])
     for path in paths:
         name=os.path.basename(path)
-        name=name[0:name.find(".")]
+        name=name[0:name.find("_")]
         result_path=os.path.join(args.results_dir,str(name))
         result_path_image=os.path.join(result_path,str(args.final_iteration)+"_0.png")
         # print(path,name,result_path,result_path_image)
